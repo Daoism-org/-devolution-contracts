f163c7f806dca0013da2d7eb04100f980174e7e1
Veronica <veronica.21.coutts@gmail.com>
2021-05-07 21:17:51 +0200

    Docs for base

--- contracts/system/base/DevolutionBase.sol
+++ contracts/system/base/DevolutionBase.sol
@@ -3,25 +3,65 @@ pragma solidity 0.7.6;
 import "../identity/IExplorer.sol";
 
 contract DevolutionBase {
+    // Interface for identity solution
     IExplorer internal explorerID_;
+    // Storage of deployer address for initial set up
+    address internal deployer_;
+
+    // -------------------------------------------------------------------------
+    // EVENTS
 
     event ExplorerJoined(address indexed explorer, uint256 explorerID);
 
-    modifier onlyOwner() {
+    // -------------------------------------------------------------------------
+    // MODIFIERS
+
+    modifier onlyExecutor() {
+        // TODO or execution address
+        require(
+            msg.sender == deployer_,
+            "Only deployer can access"
+        );
         _;
-        // TODO 
     }
 
+    // -------------------------------------------------------------------------
+    // CONSTRUCTOR
+
     constructor() {}
+
+    // -------------------------------------------------------------------------
+    // NON-STATE MODIFYING FUNCTIONS
 
     function isMember(address _explorer) external view returns(bool) {
         return explorerID_.isExplorer(_explorer);
     }
 
-    function addIdentity(address _explorer) external onlyOwner() {
+    // -------------------------------------------------------------------------
+    // STATE MODIFYING FUNCTIONS
+
+    /**
+     * @param   _explorerIdentityContract The address of the explorer identity 
+     *          solution contract. It needs to conform to the interface contract
+     *         `IExplorer`. 
+     * @notice  Only the DAO executor contract or deployer address can call this
+     *          function.
+     */
+    function addIdentityInstance(
+        address _explorerIdentityContract
+    ) 
+        external 
+        onlyExecutor() 
+    {
         explorerID_ = IExplorer(_explorer);
     }
 
+    /**
+     * @return  uint256 The token ID for the minted identity token. 
+     * @notice  This function will revert if the `addIdentityInstance` has not
+     *          been initialised with a valid instance.
+     * @dev     Emits the `ExplorerJoined` event. 
+     */
     function joinDevolution() external returns(uint256) {
         uint256 explorerID = explorerID_.mint(msg.sender);
 
